7d0296528a73e711e48b349b17f04a4724205cfd
joesoftheart <tonkha23@gmail.com>
2018-05-16 13:09:22 +0700

    Update webhooks.php
    ddjujuj

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'uqU2xwHlrBLPCJw7x3ocOVL3kcZMMY5+19IlAVZXa4H1TqsEmcDwrJkRoPgx5IJIk8beS19lKPyhYiJU8792EwZXxxNc6hXTQ/SCmXlea2kBhuH+pQbBYDHENE3KjHw0XGdi8RzxUk7QD/ZS8xQKdQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
